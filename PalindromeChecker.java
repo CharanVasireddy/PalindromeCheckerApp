b253b1a6051a682773e564244bfdbe02409f9f51
CharanVasireddy <charanchinnu0112@gmail.com>
2026-02-28 00:22:20 +0530

    UC6 added

--- PalindromeChecker.java
+++ PalindromeChecker.java
@@ -1,16 +1,23 @@
+import java.util.LinkedList;
+import java.util.Queue;
 import java.util.Stack;
+
 public class PalindromeChecker { 
     public static void main(String[] args) {
-        String input = "noon";
+        String input = "civic";
 
+        Queue<Character> queue = new LinkedList<>();
         Stack<Character> stack = new Stack<>();
+
         for (char c : input.toCharArray()) {
+            queue.offer(c);
             stack.push(c);
         }
 
         boolean isPalindrome = true;
-        for (char c : input.toCharArray()) {
-            if (c != stack.pop()) {
+
+        while (!queue.isEmpty()) {
+            if (!queue.poll().equals(stack.pop())) {
                 isPalindrome = false;
                 break;
             }
