a1f598d757e131d3cf357e22570baed4a495178c
CharanVasireddy <charanchinnu0112@gmail.com>
2026-02-28 10:36:32 +0530

    UC12 Added

--- PalindromeChecker.java
+++ PalindromeChecker.java
@@ -1,36 +1,74 @@
-class Palindrome {
+import java.util.Stack;
+import java.util.Deque;
+import java.util.LinkedList;
 
-    public boolean checkPalindrome(String input) {
-        if (input == null) {
-            return false;
+interface PalindromeStrategy {
+    boolean check(String input);
+}
+
+class StackStrategy implements PalindromeStrategy {
+
+    public boolean check(String input) {
+        String normalized = input.toLowerCase().replaceAll("\\s+", "");
+        Stack<Character> stack = new Stack<>();
+
+        for (int i = 0; i < normalized.length(); i++) {
+            stack.push(normalized.charAt(i));
         }
 
-        String normalized = input.toLowerCase().replaceAll("\\s+", "");
-
-        int left = 0;
-        int right = normalized.length() - 1;
-
-        while (left < right) {
-            if (normalized.charAt(left) != normalized.charAt(right)) {
+        for (int i = 0; i < normalized.length(); i++) {
+            if (normalized.charAt(i) != stack.pop()) {
                 return false;
             }
-            left++;
-            right--;
         }
 
         return true;
     }
 }
 
- public class PalindromeChecker {
+class DequeStrategy implements PalindromeStrategy {
+
+    public boolean check(String input) {
+        String normalized = input.toLowerCase().replaceAll("\\s+", "");
+        Deque<Character> deque = new LinkedList<>();
+
+        for (int i = 0; i < normalized.length(); i++) {
+            deque.addLast(normalized.charAt(i));
+        }
+
+        while (deque.size() > 1) {
+            if (!deque.removeFirst().equals(deque.removeLast())) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+}
+
+class PalindromeService {
+
+    private PalindromeStrategy strategy;
+
+    public PalindromeService(PalindromeStrategy strategy) {
+        this.strategy = strategy;
+    }
+
+    public boolean execute(String input) {
+        return strategy.check(input);
+    }
+}
+
+public class PalindromeChecker {
 
     public static void main(String[] args) {
 
         String input = "Never Odd Or Even";
 
-        Palindrome checker = new Palindrome();
+        PalindromeStrategy strategy = new StackStrategy();
+        PalindromeService service = new PalindromeService(strategy);
 
-        boolean result = checker.checkPalindrome(input);
+        boolean result = service.execute(input);
 
         System.out.println("Input String : " + input);
 
